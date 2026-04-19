3dc424d2ec913a8868944ef151f645f90609b589
Nina Py <ninapy@Ninas-MacBook-Air-8.local>
2026-04-19 13:27:56 -0400

    feat: transcriber

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Interactive-Podcasts
 Enhanced Listening: Interactive Podcasts. This tool uses AI (Whisper &amp; Gemini) to transform standard audio files into active learning experiences. It automatically generates and inserts comprehension questions and a final summary into your audio, utilizing active recall to improve retention.
+
+
+
+Is Your Personality Fixed, Or Can You Change Who You Are (Invisibilia NPR)
+https://www.youtube.com/watch?v=nMmiXaER9ko
---
+++ transcriber.py
@@ -0,0 +1,36 @@
+# source: https://developers.openai.com/api/docs/guides/speech-to-text
+
+import json
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if not os.getenv("OPENAI_API_KEY"):
+    raise ValueError("OPENAI_API_KEY not found on .env file")
+
+client = OpenAI()
+audio_file = open("episode.mp3", "rb") # read binary
+
+transcription = client.audio.transcriptions.create(
+    file=audio_file,
+    model="whisper-1",
+    response_format="verbose_json",
+    timestamp_granularities=["word"]
+    # prompt="might include prompting later for better results"
+)
+
+output = {
+    "text": transcription.text,
+    "words": [
+        {"word": w.word, "start": w.start, "end": w.end}
+        for w in transcription.words
+    ]
+}
+
+output_file = open("transcript.json", "w") # write
+json.dump(output, output_file)
+output_file.close()
+
+print(transcription.words)
